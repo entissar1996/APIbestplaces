658394be78313eef61d08390f442014706738d96
Entissar BEN RAJEB <entissarbenrajeb@gmail.com>
2021-11-03 15:59:19 +0100

    heroku update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
+const multer=require('multer')
 
 
 const usersRouter = require('./auth/users');
@@ -43,5 +44,39 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({message:'500 Server error :('});
 });
+//images upload
+app.use('/uploads', express.static(__dirname +'/uploads'));
+ var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().toISOString()+file.originalname)
+    }
+  })
 
+  var upload = multer({ storage: storage })
+  app.post('/upload', upload.single('myFile'), async(req, res, next) => {
+    const file = req.file
+    if (!file) {
+      const error = new Error('Please upload a file')
+      error.httpStatusCode = 400
+      return next("hey error")
+    }
+
+
+      const imagepost= new model({
+        image: file.path
+      })
+      const savedimage= await imagepost.save()
+      res.json(savedimage)
+
+  })
+
+  app.get('/image',async(req, res)=>{
+   const image = await model.find()
+   res.json(image)
+
+  })
+  app.use('/uploads', express.static(__dirname +'/uploads'));
 module.exports = app;
