0cd228bb9df72a9e9df6ee4dac6e74d53c1a725f
Entissar BEN RAJEB <entissarbenrajeb@gmail.com>
2021-11-04 14:25:23 +0100

    image upload

--- server.js
+++ server.js
@@ -4,14 +4,9 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
-const multer=require('multer')
-
+const multer = require('multer');
 
 const usersRouter = require('./auth/users');
-
-
-
-
 
 // setting up express app
 const app = express();
@@ -22,61 +17,56 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-
 // api endpoint
 app.use('/users', usersRouter);
 
-
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404)) ;
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json({message:'500 Server error :('});
+  res.json({message: '500 Server error :('});
 });
-//images upload
-app.use('/uploads', express.static(__dirname +'/uploads'));
- var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, new Date().toISOString()+file.originalname)
+// images upload
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+var upload = multer({ storage: storage });
+app.use(express.static(__dirname + '/public'));
+app.use('/uploads', express.static('uploads'));
+
+app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
+  // req.file is the `profile-file` file
+  // req.body will hold the text fields, if there were any
+  console.log(JSON.stringify(req.file))
+  var response = req.file.path;
+
+  return res.send(response)
+});
+
+/* app.post('/profile-upload-multiple', upload.array('profile-files', 12), function (req, res, next) {
+    // req.files is array of `profile-files` files
+    // req.body will contain the text fields, if there were any
+    console.log(JSON.stringify(req.file))
+
+    for(var i=0;i<req.files.length;i++){
+       var response += req.files[i].path;
     }
-  })
 
-  var upload = multer({ storage: storage })
-  app.post('/upload', upload.single('myFile'), async(req, res, next) => {
-    const file = req.file
-    if (!file) {
-      const error = new Error('Please upload a file')
-      error.httpStatusCode = 400
-      return next("hey error")
-    }
+    return res.send(response)
+}) */
 
-
-      const imagepost= new model({
-        image: file.path
-      })
-      const savedimage= await imagepost.save()
-      res.json(savedimage)
-
-  })
-
-  app.get('/image',async(req, res)=>{
-   const image = await model.find()
-   res.json(image)
-
-  })
-  app.use('/uploads', express.static(__dirname +'/uploads'));
 module.exports = app;
