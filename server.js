4b8671afaf09702246e746279a1ebb26b906e686
Entissar BEN RAJEB <entissarbenrajeb@gmail.com>
2021-11-21 10:59:46 +0100

    upload fix

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ var storage = multer.diskStorage({
   }
 });
 var upload = multer({ storage: storage });
-app.use(express.static(__dirname + '/public'));*/
-app.use('/uploads', express.static('uploads'));
-app.use('/uploadsavatar', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(__dirname + '/public'));
+app.use('/uploads', express.static('uploads'));*/
+app.use('/uploadsavatar', express.static(path.join(__dirname, 'uploads')),avatarRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/posts', postRouter);
 app.use('/comments', commentsRouter);
